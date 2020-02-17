8725a06fb3147750ceb7f9d09e3a93b994d082b2
Josh Gude <gude.joshua@gmail.com>
2020-02-16 21:16:07 -0500

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Josh Gude on 2/16/20.
+//  Copyright © 2020 Josh Gude. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
